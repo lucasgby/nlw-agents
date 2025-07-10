c1eb7516edf1c61bdbe68c04d1eb0b488fcbd58d
lugyll <lucas.gabdev@gmail.com>
2025-07-10 19:31:32 -0300

    created server

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+
+import { env } from './env';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return 'Ok';
+});
+
+app.listen({ port: env.PORT });
