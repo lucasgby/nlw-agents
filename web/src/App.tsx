865b8b4027d810bc06c499443da3fa2777c02b23
lugyll <lucas.gabdev@gmail.com>
2025-07-10 23:02:12 -0300

    configure routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,20 @@
+import { CreateRoom } from "@/pages/create-room";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { Room } from "@/pages/room";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <div>Hello world</div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
